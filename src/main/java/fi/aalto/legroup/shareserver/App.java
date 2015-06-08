4fe8f21f9072bbba3afebd00a76ab8ba79943876
bqqbarbhg <samuli.1995@hotmail.com>
2015-06-08 16:16:37 +0300

    List files

--- src/main/java/fi/aalto/legroup/shareserver/App.java
+++ src/main/java/fi/aalto/legroup/shareserver/App.java
@@ -2,11 +2,13 @@ package fi.aalto.legroup.shareserver;
 
 import java.io.IOException;
 import java.io.OutputStream;
+import java.io.File;
 import java.net.InetSocketAddress;
 
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.Headers;
 
 /**
  * Hello world!
@@ -24,12 +26,62 @@ public class App
 
     static class RootHandler implements HttpHandler {
 
+        private String root = new String("D:/shareserver");
+
         public void handle(HttpExchange t) throws IOException {
-            String response = t.getRequestMethod() + " " + t.getRequestURI().toString() + "\n";
-            t.sendResponseHeaders(200, response.length());
-            OutputStream out = t.getResponseBody();
-            out.write(response.getBytes());
-            out.close();
+
+            String uri = t.getRequestURI().toString();
+            String method = t.getRequestMethod();
+
+            if (uri.matches("[A-Za-z0-9/]*(\\.[A-Za-z0-9]+)?")) {
+
+                if (method.equals("GET")) {
+
+                    File file = new File(root + uri);
+
+                    if (!file.exists()) {
+                        String response = "{ \"error\": \"Path not found\" }\n";
+                        Headers headers = t.getResponseHeaders();
+                        headers.set("Content-Type", "application/json");
+                        t.sendResponseHeaders(404, response.length());
+                        OutputStream out = t.getResponseBody();
+                        out.write(response.getBytes());
+                        out.close();
+                        return;
+                    }
+
+                    if (file.isDirectory()) {
+
+                        File[] files = file.listFiles();
+
+                        String response = "{ \"directory\": [";
+
+                        for (File child : files) {
+
+                            response += "{ \"name\": \"" + child.getName() + "\", ";
+                            response += "\"modified\": \"" + child.lastModified() + "\" },\n";
+                        }
+                        response = response.substring(0, response.length() - 2);
+                        response += "] }\n";
+
+                        Headers headers = t.getResponseHeaders();
+                        headers.set("Content-Type", "application/json");
+                        t.sendResponseHeaders(200, response.length());
+                        OutputStream out = t.getResponseBody();
+                        out.write(response.getBytes());
+                        out.close();
+                    }
+                }
+
+            } else {
+                String response = "{ \"error\": \"Invalid url\", \"url\": \"" + uri + "\" }\n";
+                Headers headers = t.getResponseHeaders();
+                headers.set("Content-Type", "application/json");
+                t.sendResponseHeaders(404, response.length());
+                OutputStream out = t.getResponseBody();
+                out.write(response.getBytes());
+                out.close();
+            }
         }
     }
 }
