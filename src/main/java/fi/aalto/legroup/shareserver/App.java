3314a913c3548793f41d5eb719bb9af97d755d4b
bqqbarbhg <samuli.1995@hotmail.com>
2015-06-08 15:47:25 +0300

    Mirror HTTP requests

--- src/main/java/fi/aalto/legroup/shareserver/App.java
+++ src/main/java/fi/aalto/legroup/shareserver/App.java
@@ -1,13 +1,35 @@
 package fi.aalto.legroup.shareserver;
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
 /**
  * Hello world!
- *
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main(String[] args) throws IOException
     {
-        System.out.println( "Hello World!" );
+        HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
+        server.createContext("/", new RootHandler());
+        server.setExecutor(null);
+        server.start();
+
+    }
+
+    static class RootHandler implements HttpHandler {
+
+        public void handle(HttpExchange t) throws IOException {
+            String response = t.getRequestMethod() + " " + t.getRequestURI().toString() + "\n";
+            t.sendResponseHeaders(200, response.length());
+            OutputStream out = t.getResponseBody();
+            out.write(response.getBytes());
+            out.close();
+        }
     }
 }
